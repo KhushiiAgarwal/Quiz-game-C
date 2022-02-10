4cb1dc61603d387a14bdbcf79a120a978d626143
sanskrutikhedkar9 <99036043+sanskrutikhedkar9@users.noreply.github.com>
2022-02-10 12:00:14 +0530

    Add files via upload

---
+++ quizgamepsap (1).c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(){
+
+    //INTRODUCTION- Sanskruti
+    printf("\t\t\t  C PROGRAM QUIZ GAME\n");
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t\t    WELCOME ");
+    printf("\n\t\t\t      to ");
+    printf("\n\t\t\t    THE GAME ");
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t\t    PLAY QUIZZES ");
+    printf("\n\t\t\t      and ");
+    printf("\n\t\t\t    EARN POINTS ");
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t > Press S to  START the game");
+    printf("\n\t\t > Press U for SPORTS QUIZ             ");
+    printf("\n\t\t > Press U for ENTERTAINMENT QUIZ             ");
+    printf("\n\t\t > Press H for HELP             ");
+    printf("\n\t\t > Press E to  EXIT             ");
+    printf("\n\t\t________________________________________\n\n");
+    //MAIN MENU- Khushi
+   
+
+    //HELP- Sanskruti
+    printf("\n\n                               HELP");
+    printf("\n ------------------  Welcome to C Program Quiz Game --------------------------");
+    printf("\n\n Here are some tips you might wanna know before playing:");
+    printf("\n\t  > Game consists of 2 categories: ");
+    printf("\n\t\t  1. Sports Quiz");
+    printf("\n\t\t  2. Entertainment Quiz ");
+    printf("\n\t  > Each category comprises 3 rounds: ");
+    printf("\n\t\t  1. Easy ");
+    printf("\n\t\t  2. Medium"); 
+    printf("\n\t\t  3. Difficult");
+    printf("\n\t  > Each round will have 10 questions."); 
+    printf("\n\t  > For each question you will be given 4 options and you have to press A, B ,C or D for the right option.");
+    printf("\n\t  > 10 points will be awarded for each right answer.");
+    printf("\n\t  > No negative points for wrong answer."); 
+    printf("\n\t  > Final score will be available after each round."); 
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t________________________________________");
+    printf("\n\n\t!!!!!!!!!!!!! ALL THE BEST !!!!!!!!!!!!!!!!!");
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t________________________________________");
+    printf("\n\t\t > Press P for the final score");
+    printf("\n\t\t > Press Y to  START the game");
+    printf("\n\t\t > Press Z to  return  to the MAIN MENU ");
+    //MENU- Khushi
+ 
+    return 0;
+}
